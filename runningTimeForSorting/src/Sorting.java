a84927441e3d3fe52546e1fad065d657374e00d6
marvinamari <contact@marvinamari.com>
2017-11-04 23:41:06 -0400

    Added optimized bubble sort

--- runningTimeForSorting/src/Sorting.java
+++ runningTimeForSorting/src/Sorting.java
@@ -7,6 +7,22 @@ import java.sql.Time;
 
     public class Sorting {
 
+    public void optimizedBubbleSort(int arr[])
+    {
+
+    int size = arr.length;
+    for (int i = 0; i < size-1; i++)
+        for (int j = 0; j < size-i-1; j++)
+            if (arr[j] > arr[j+1])
+            {
+                // swap temp and arr[i]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+ 
+    }
+
     public void bubbleSort(int arr[])
     {
 
